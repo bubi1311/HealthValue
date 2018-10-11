6818aab1ae19f49b892185acb275905259cab204
Stefan <bubi1311@antispam.de>
2018-10-11 21:28:28 +0200

    Added ValidatorErrorMessages class and a (empty) unittest for it.

---
+++ app/src/main/java/de/clever/healthvalue/util/validators/ValidatorErrorMessages.java
@@ -0,0 +1,31 @@
+package de.clever.healthvalue.util.validators;
+
+import android.widget.EditText;
+
+public class ValidatorErrorMessages {
+
+ 
+
+    /**
+     * Sets an custom error message to an editText
+     *
+     * @param editText - The EditText-Object.
+     * @param errorString - The error message.
+     */
+    public static void setError(EditText editText, String errorString) {
+
+        editText.setError(errorString);
+
+    }
+
+    /**
+     * Clears a former set error message o
+     *
+     * @param editText - The EditText-object that should be cleaned.
+     */
+    public static void clearError(EditText editText) {
+
+        editText.setError(null);
+
+    }
+}
---
+++ app/src/test/java/de/clever/healthvalue/util/validators/ValidatorErrorMessagesTest.java
@@ -0,0 +1,18 @@
+package de.clever.healthvalue.util.validators;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidatorErrorMessagesTest {
+
+    @Test
+    public void setError() {
+        // TODO Implement!
+    }
+
+    @Test
+    public void clearError() {
+        // TODO Implement!
+    }
+}
