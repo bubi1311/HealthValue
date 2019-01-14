832e9b93118ed945f08550b3654ef242d336daff
stefan <bubi1311@antispam.de>
2019-01-14 21:56:01 +0100

    Implemented the getTemperature(int id) method.

--- app/src/main/java/de/clever/healthvalue/dao/TemperatureDao.java
+++ app/src/main/java/de/clever/healthvalue/dao/TemperatureDao.java
@@ -2,13 +2,16 @@ package de.clever.healthvalue.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
 import de.clever.healthvalue.entities.Temperature;
+import de.clever.healthvalue.util.DateTimeUtils;
 import de.clever.healthvalue.util.HealthValueDbHelper;
 
 public class TemperatureDao {
@@ -17,6 +20,7 @@ public class TemperatureDao {
     private SQLiteDatabase database;
 
     private HealthValueDbHelper dbHelper;
+    private static String dateTimePattern = "yyyy-MM-dd HH:mm:ss";
 
 
     public TemperatureDao(Context context) {
@@ -40,22 +44,50 @@ public class TemperatureDao {
         ContentValues values = new ContentValues();
         values.put(HealthValueDbHelper.COLUMN_TEMPERATURE_BODYTEMPERATURE, temperature.getBodyTemperature());
         values.put(HealthValueDbHelper.COLUMN_TEMPERATURE_SCALE, temperature.getScale());
-        values.put(HealthValueDbHelper.COLUMN_TEMPERATURE_TIMESTAMP, temperature.getDateTime());
+        values.put(HealthValueDbHelper.COLUMN_TEMPERATURE_TIMESTAMP, temperature.getDateTime(dateTimePattern));
 
-        database.insert(HealthValueDbHelper.TABLE_TEMPERATURE,null,values);
+        database.insert(HealthValueDbHelper.TABLE_TEMPERATURE, null, values);
         database.close();
     }
 
-    public Temperature getTemperature(int id){
-        throw new UnsupportedOperationException("Not implemented yet");
+    public Temperature getTemperature(int id) {
         // see https://o7planning.org/de/10433/die-anleitung-zu-android-sqlite-database
+
+
+        SQLiteDatabase dbRead = dbHelper.getReadableDatabase();
+
+        Cursor cursor = dbRead.query(HealthValueDbHelper.TABLE_TEMPERATURE, new String[]{
+                        HealthValueDbHelper.COLUMN_TEMPERATURE_ID, HealthValueDbHelper.COLUMN_TEMPERATURE_BODYTEMPERATURE,
+                        HealthValueDbHelper.COLUMN_TEMPERATURE_SCALE, HealthValueDbHelper.COLUMN_TEMPERATURE_TIMESTAMP},
+                HealthValueDbHelper.COLUMN_TEMPERATURE_ID + "=?", new String[]{String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Temperature t = null;
+        try {
+            t = new Temperature(Long.parseLong(cursor.getString(0)), Double.parseDouble(cursor.getString(1)), cursor.getString(2), DateTimeUtils.getParsedDateTimeFromLocalizedString(cursor.getString(3), dateTimePattern));
+        } catch (ParseException e) {
+            Log.e(LOG_TAG, "Exception parsing the database content to a temperature object. Message: " +e.getLocalizedMessage());
+        }
+        catch (NullPointerException npe)
+        {
+            Log.e(LOG_TAG, "Exception parsing the database content to a temperature object. Message: " +npe.getLocalizedMessage());
+        }
+        finally {
+            if(cursor != null)
+                cursor.close();
+        }
+        return t;
+
+
     }
 
-    public List<Temperature> getAllTemperatures(){
+    public List<Temperature> getAllTemperatures() {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
-    public List<Temperature> getTemperaturesBetweenDates(Date from, Date until){
+    public List<Temperature> getTemperaturesBetweenDates(Date from, Date until) {
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
