b48351220e552512b68c0c110b652f7087d86a5a
stefan <bubi1311@antispam.de>
2019-01-14 22:24:15 +0100

    Moved the method getParsedDateTimeFromLocalizedString and changed the signature of getDateTimeAsLocalizedString (added the pattern as an attribute).

--- app/src/main/java/de/clever/healthvalue/util/DateTimeUtils.java
+++ app/src/main/java/de/clever/healthvalue/util/DateTimeUtils.java
@@ -1,26 +1,45 @@
 package de.clever.healthvalue.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class DateTimeUtils {
 
-    public static String dateTimePattern = "yyyy-MM-dd HH:mm:ss";
+
 
     private DateTimeUtils(){
 
     }
 
+
     /**
-     * Get the date and time as a string in local format.
+     * public static String dateTimePattern = "yyyy-MM-dd HH:mm:ss";
      *
      * @param datetime - The Date-object that should be returned as a String-object.
+     * @param pattern - Pattern describing the date and time format.
      * @return localized date and time where the temperature was added as string.
      */
-    public static String getDateTimeAsLocalizedString(Date datetime) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat(dateTimePattern, Locale.getDefault());
+    public static String getDateTimeAsLocalizedString(Date datetime, String pattern) {
+        SimpleDateFormat dateFormat;
+        dateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
         return dateFormat.format(datetime);
+    }
+
+    /**
+     * Returns a date object which was parsed from a string using the patternFormat.
+     * @param dateTime - The String-object that should parsed to a date object.
+     * @param patternFormat - the pattern describing the date and time format is should be parsed from.
+     * @return an date object from a formatted string.
+     * @throws ParseException - Please catch the parse exception in your code.
+     */
+    public static Date getParsedDateTimeFromLocalizedString(String dateTime, String patternFormat) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(patternFormat, Locale.getDefault());
+        Date myDate = sdf.parse(dateTime);
+
+
+        return myDate;
     }
 
 }
